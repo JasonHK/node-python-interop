ac2fd0f656e664bf92b0d82f3197dc5d509a62d5
Jason Kwok <JasonHK@users.noreply.github.com>
2021-06-12 17:42:03 +0800

    feat: added more Python functions

--- src/npi.cpp
+++ src/npi.cpp
@@ -23,11 +23,15 @@ namespace NPI
 
     Napi::Value StartInterpreter(const Napi::CallbackInfo&);
 
+    Napi::Value AppendSysPath(const Napi::CallbackInfo& info);
+
     Napi::Value Import(const Napi::CallbackInfo&);
 
     Napi::Value Eval(const Napi::CallbackInfo&);
 
     Napi::Value Dir(const Napi::CallbackInfo&);
+
+    Napi::Value GetAttr(const Napi::CallbackInfo&);
 }
 
 Napi::Object NPI::Init(Napi::Env env, Napi::Object exports)
@@ -36,9 +40,11 @@ Napi::Object NPI::Init(Napi::Env env, Napi::Object exports)
 
     exports.Set("dlOpen", Function::New(env, DlOpen, STRINGIFY(DlOpen)));
     exports.Set("startInterpreter", Function::New(env, StartInterpreter, STRINGIFY(StartInterpreter)));
+    exports.Set("appendSysPath", Function::New(env, AppendSysPath, STRINGIFY(AppendSysPath)));
     exports.Set("import", Function::New(env, Import, STRINGIFY(Import)));
     exports.Set("eval", Function::New(env, Eval, STRINGIFY(Eval)));
     exports.Set("dir", Function::New(env, Dir, STRINGIFY(Dir)));
+    exports.Set("getattr", Function::New(env, GetAttr, STRINGIFY(GetAttr)));
 
     WrappedPythonObject::Init(env, exports);
 
@@ -65,9 +71,19 @@ Napi::Value NPI::DlOpen(const Napi::CallbackInfo& info)
 
 using NPI::PythonEnsureGil;
 
+static wchar_t *program = NULL;
+
 Napi::Value NPI::StartInterpreter(const Napi::CallbackInfo& info)
 {
     auto env = info.Env();
+
+    if ((program != NULL) && (info.Length() >= 1) && info[0].IsString())
+    {
+        auto path = info[0].As<Napi::String>().Utf8Value();
+
+        program = Py_DecodeLocale(path.c_str(), NULL);
+        Py_SetProgramName(program);
+    }
 
     if (!Py_IsInitialized())
     {
@@ -85,6 +101,26 @@ Napi::Value NPI::StartInterpreter(const Napi::CallbackInfo& info)
 #endif
 
     PyEval_SaveThread();
+
+    return env.Undefined();
+}
+
+Napi::Value NPI::AppendSysPath(const Napi::CallbackInfo& info)
+{
+    auto env = info.Env();
+    EnsurePythonInitialized(env);
+
+    auto path = info[0].As<Napi::String>().Utf8Value();
+
+    {
+        PythonEnsureGil _;
+
+        auto p_path = PyUnicode_FromString(path.c_str());
+
+        auto p_paths = PySys_GetObject("path");
+        PyList_Append(p_paths, p_path);
+        Py_DECREF(p_path);
+    }
 
     return env.Undefined();
 }
@@ -128,14 +164,53 @@ Napi::Value NPI::Eval(const Napi::CallbackInfo& info)
 
     auto program = info[0].As<Napi::String>().Utf8Value();
 
-    int exit_code;
     {
         PythonEnsureGil _;
 
-        exit_code = PyRun_SimpleString(program.c_str());
-    }
+        bool has_frame = (PyEval_GetFrame() != NULL);
 
-    return Napi::Number::New(env, exit_code);
+        PyObject* globals;
+        if ((info.Length() >= 1) && !IsNullLike(info[1]))
+        {
+            globals = ToPythonObject(info[1]);
+        }
+        else
+        {
+            globals = has_frame ? PyEval_GetGlobals() : PyModule_GetDict(PyImport_AddModule("__main__"));
+        }
+
+        PyObject* locals;
+        if ((info.Length() >= 2) && !IsNullLike(info[2]))
+        {
+            locals = ToPythonObject(info[2]);
+        }
+        else
+        {
+            locals = has_frame ? PyEval_GetLocals() : globals;
+        }
+
+        PyObject* p_return = PyRun_String(program.c_str(), Py_eval_input, globals, locals);
+        if (p_return == NULL)
+        {
+            // PyErr_Print();
+
+            PyObject* error_type;
+            PyObject* error_value;
+            PyObject* error_trace;
+
+            PyErr_Fetch(&error_type, &error_value, &error_trace);
+
+            auto error = Napi::Error::New(env, PyUnicode_AsUTF8(PyObject_Str(error_value)));
+            error.Set("name", ((PyTypeObject*) error_type)->tp_name);
+
+            throw error;
+        }
+
+        auto n_return = ToNodeValue(env, p_return);
+        Py_DECREF(p_return);
+
+        return n_return;
+    }
 }
 
 Napi::Value NPI::Dir(const Napi::CallbackInfo& info)
@@ -143,10 +218,30 @@ Napi::Value NPI::Dir(const Napi::CallbackInfo& info)
     auto env = info.Env();
     EnsurePythonInitialized(env);
 
-    auto python_target = ToPythonValue(info[0]);
-    auto python_keys   = PyObject_Dir(python_target);
+    {
+        PythonEnsureGil _;
 
-    return ToNodeValue(env, python_keys);
+        auto python_target = ToPythonObject(info[0]);
+        auto python_keys   = PyObject_Dir(python_target);
+
+        return ToNodeValue(env, python_keys);
+    }
+}
+
+Napi::Value NPI::GetAttr(const Napi::CallbackInfo& info)
+{
+    auto env = info.Env();
+    EnsurePythonInitialized(env);
+
+    {
+        PythonEnsureGil _;
+
+        auto python_target = ToPythonObject(info[0]);
+        auto python_name   = ToPythonObject(info[1]);
+        auto python_value  = PyObject_GetAttr(python_target, python_name);
+
+        return ToNodeValue(env, python_value);
+    }
 }
 
 Napi::Value NPI::Symbols::Repr(Napi::Env env)
